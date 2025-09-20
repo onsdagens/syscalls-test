8837894650dc34566fab27274ceb4ee98e1633f9
Malte Munch <malte.munch@ltu.se>
2025-09-20 10:18:15 +0200

    suspicious changes
    Signed-off-by: Malte Munch <malte.munch@ltu.se>

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,27 @@
 
 
 pub fn my_syscall(buf: &[u8; 64]) {
+    #[cfg(target_arch = "aarch64")]
     unsafe {
         core::arch::asm!("
             
-            mov x8, 64 // replace these with x86 equivalent
+            mov x8, 64 // aarch
             mov x0, 1
             mov x1, {buf}
             mov x2, {count}
             svc 0",
+            buf = in(reg) buf,
+            count = in(reg) buf.len());
+    }
+    
+    #[cfg(target_arch = "x86_64")]
+    unsafe {
+        core::arch::asm!("
+            
+            mov rax, 1 // x86
+            mov rbx, {buf}
+            mov rcx, {count}
+            syscall",
             buf = in(reg) buf,
             count = in(reg) buf.len());
     }
--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,22 @@ extern "C" fn main() {
     buf[0] = 'h' as u8;
     buf[1] = 'i' as u8;
     syscalls_test::my_syscall(&buf);
+
+    #[cfg(target_arch = "aarch64")]
     unsafe{core::arch::asm!("
-                // replace these with x86 equivalent
                 mov x8, 94
                 mov x0, 19
                 svc 0
-                            ")}
+   ")}
+    
+    #[cfg(target_arch = "x86_64")]
+    unsafe{core::arch::asm!("
+                // write
+                mov rax,1 
+                // stdout
+                mov rbx,0
+                syscall
+   ")}
 }
 
 #[panic_handler]
@@ -30,6 +40,18 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 fn abort() -> ! {
+    let mut buf: [u8; 64] = [0u8; 64];
+    buf[0] = 'a' as u8;
+    buf[1] = 'b' as u8;
+    buf[2] = 'o' as u8;
+    buf[3] = 'r' as u8;
+    buf[4] = 't' as u8;
+    syscalls_test::my_syscall(&buf);
+    loop{}
+}
+
+#[unsafe(no_mangle)]
+fn memset() -> ! {
     let mut buf: [u8; 64] = [0u8; 64];
     buf[0] = 'a' as u8;
     buf[1] = 'b' as u8;
